1f7dfd04c385e61aba00855f5342a2fa50d7d2f8
Sayah Reda <reda1998.rs@gmail.com>
2022-03-07 17:39:32 +0100

    feat:add header component

---
+++ src/component/header.tsx
@@ -0,0 +1,42 @@
+/* This example requires Tailwind CSS v2.0+ */
+import React from 'react';
+import logo from '../imgs/Logo.svg';
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Link } from 'react-router-dom';
+
+const navigation = [
+  { name: 'About', href: '#' },
+  { name: 'Experience', href: '#' },
+  { name: 'Project', href: '#' },
+  { name: 'Contact', href: '#' }
+];
+
+export default function Example() {
+  return (
+    <div className="relative flex justify-between  py-10 px-20">
+      <div className="">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="flex justify-between w-1/2 pr-8">
+        <ul className="flex gap-16  ">
+          {navigation.map((element, index) => {
+            return (
+              <li key={element.name}>
+                <span className="text-base text-white">
+                  {' '}
+                  <span className="text-bermuda pr-1">{index + 1}.</span>
+                  {element.name}
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+        <p className="p-resume  text-bermuda border px-2 py-1 rounded">
+          Resume
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      purple: '#3f3cbb',
+      midnight: '#121063',
+      metal: '#565584',
+      tahiti: '#3ab7bf',
+      silver: '#ecebff',
+      'bubble-gum': '#ff77e9',
+      bermuda: '#64FFDA'
+    },
+    extend: {
+      height: {
+        '1h': '100vh'
+      }
+    }
+  },
+  plugins: []
+};
